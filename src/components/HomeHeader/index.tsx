c2d3597bca5b891ac36a9b7369dbfefc22031e94
João Alberto <joaoalbertosv15@gmail.com>
2022-11-24 01:11:49 -0300

    feat: adicionando as configurações das rotas privadas

---
+++ src/components/HomeHeader/index.tsx
@@ -0,0 +1,15 @@
+import { Heading, HStack, Text } from "native-base";
+
+
+export function HomeHeader() {
+  return (
+    <HStack>
+      <Text color="gray.100">
+        olá,
+      </Text>
+      <Heading color="gray.100" >
+        João Albert
+      </Heading>
+    </HStack>
+  )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,12 @@
+import { HomeHeader } from "@components/HomeHeader";
 import { Center, Text } from "native-base";
 
 
 export function Home() {
 
   return (
-    <Center flex={1}>
-      <Text>Home</Text>
+    <Center flex={1} >
+     <HomeHeader />
     </Center>
   )
 }
